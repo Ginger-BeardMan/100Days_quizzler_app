c2c77ac382bf14459ffc2b4279ececcf67e6d1a7
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-11-23 13:41:46 -0500

    Update ui.py
    added the ability to show each question in the window canvas
    
    added a check for the correct answer and to update the score based on whether the answer is correct or not.

--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
@@ -13,17 +15,34 @@ class QuizInterface:
         truth_check = PhotoImage(file='./images/true.png')
         false_x = PhotoImage(file='./images/false.png')
 
-        self.q_score = Label(text='Score: 0', bg=THEME_COLOR, fg='white', font=('Ariel', 10, 'normal'))
+        self.q_score = Label(text=f'Score: {self.quiz.score}', bg=THEME_COLOR, fg='white', font=('Ariel', 10, 'normal'))
         self.q_score.grid(column=1, row=0)
 
         self.q_canvas = Canvas(height=250, width=300)
-        self.q_canvas.create_text(125, 150, text='', font=('Ariel', 20, 'italic'))
+        self.question_text = self.q_canvas.create_text(150, 125, text='Some Text', font=('Ariel', 20, 'italic'),
+                                                       fill=THEME_COLOR, width=280)
         self.q_canvas.grid(column=0, row=1, columnspan=2, pady=20, padx=20)
 
-        self.true_button = Button(image=truth_check, pady=20, highlightthickness=0)
+        self.true_button = Button(image=truth_check, pady=20, highlightthickness=0, command=self.answer_is_true)
         self.true_button.grid(column=0, row=2)
 
-        self.false_button = Button(image=false_x, pady=20, highlightthickness=0)
+        self.false_button = Button(image=false_x, pady=20, highlightthickness=0, command=self.answer_is_false)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.q_canvas.itemconfig(self.question_text, text=q_text)
+
+    def answer_is_true(self):
+        self.quiz.check_answer('true')
+        self.get_next_question()
+        self.q_score.config(text=f'Score: {self.quiz.score}')
+
+    def answer_is_false(self):
+        self.quiz.check_answer('false')
+        self.get_next_question()
+        self.q_score.config(text=f'Score: {self.quiz.score}')
