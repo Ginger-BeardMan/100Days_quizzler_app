7683fc76aa7b74c0b9a1b00dd66e393c51f0f487
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-11-25 06:59:02 -0500

    Update ui.py
    fixed bug for end game conditions, set a message to display when all questions have been answered.

--- ui.py
+++ ui.py
@@ -34,23 +34,25 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.q_canvas.itemconfig(self.question_text, text=q_text)
         self.q_canvas.configure(bg='white')
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.q_canvas.itemconfig(self.question_text, text=q_text)
+            self.q_score.config(text=f'Score: {self.quiz.score}')
+        else:
+            self.q_canvas.itemconfig(self.question_text, text='You have reached the end of the quiz.')
+            self.true_button.config(state='disabled')
+            self.false_button.config(state='disabled')
 
     def answer_is_true(self):
         self.give_feedback(self.quiz.check_answer('true'))
-        self.q_score.config(text=f'Score: {self.quiz.score}')
 
     def answer_is_false(self):
         self.give_feedback(self.quiz.check_answer('false'))
-        self.q_score.config(text=f'Score: {self.quiz.score}')
 
     def give_feedback(self, is_right):
         if is_right:
             self.q_canvas.configure(bg='green')
-            self.window.after(1000, self.get_next_question)
         else:
             self.q_canvas.configure(bg='red')
-            self.window.after(1000, self.get_next_question)
-
+        self.window.after(1000, self.get_next_question)
